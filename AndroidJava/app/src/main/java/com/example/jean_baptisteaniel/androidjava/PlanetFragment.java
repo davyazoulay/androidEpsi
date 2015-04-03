6237f95db1949c9f11d1dcb758969d17c0e53cb2
jb <jb@apptamin.com>
2015-04-03 17:20:14 +0200

    commit init

---
+++ AndroidJava/app/src/main/java/com/example/jean_baptisteaniel/androidjava/PlanetFragment.java
@@ -0,0 +1,9 @@
+package com.example.jean_baptisteaniel.androidjava;
+
+import android.app.Fragment;
+
+/**
+ * Created by jean-baptisteaniel on 03/04/15.
+ */
+public class PlanetFragment extends Fragment {
+}
---
+++ AndroidJava/app/src/main/java/com/example/jean_baptisteaniel/androidjava/principal_view.java
@@ -0,0 +1,25 @@
+package com.example.jean_baptisteaniel.androidjava;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.jean_baptisteaniel.androidjava.R;
+
+
+public class principal_view extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.principal_activity);
+    }
+}
