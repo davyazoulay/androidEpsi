2f508ea4b85efe2e9e4e5085009dc2f545928e63
unknown <davyazoulay@users.noreply.github.com>
2015-08-06 15:45:32 +0200

    ajout singlestring

---
+++ SportFounder2/app/src/main/java/com/example/jean_baptisteaniel/sportfounder2/Models/SingleString.java
@@ -0,0 +1,18 @@
+package com.example.jean_baptisteaniel.sportfounder2.Models;
+
+/**
+ * Created by davy.azoulay on 04/08/2015.
+ */
+public class SingleString {
+    private String Texte;
+
+    public SingleString(String txt){Texte=txt;}
+
+    public String getTexte() {
+        return Texte;
+    }
+
+    public void setTexte(String texte) {
+        Texte = texte;
+    }
+}
---
+++ SportFounder2/app/src/main/java/com/example/jean_baptisteaniel/sportfounder2/Tools.java
@@ -0,0 +1,16 @@
+package com.example.jean_baptisteaniel.sportfounder2;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by davy.azoulay on 05/08/2015.
+ */
+public class Tools {
+    public static Date getCurrentDate(){
+        //DateFormat dateFormat = new SimpleDateFormat("yyyy'-'MM'-'dd'T'HH':'mm':'ss");
+        Date date = new Date();
+        return date;
+    }
+}
